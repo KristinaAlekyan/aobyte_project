91b84e14b98ee5089ca51b894ff33588d5f6b417
Kristina Alekyan <kristinaalekyan1@gmail.com>
2022-06-20 13:06:40 +0400

    Fixed merging conflicts

--- src/components/Beverages/Beverages.js
+++ src/components/Beverages/Beverages.js
@@ -2,26 +2,6 @@ import { useState, useEffect } from 'react';
 import Product from '../Product/Product';
 import '../Beverages/Beverages.css';
 
-<<<<<<< HEAD
-=======
-const beveragesData =   [
-                            {
-                                "id": 15,
-                                "product_name":"Coca-Cola",
-                                "product_price":"9000",
-                                "category": "Beverages",
-                                "image": "/images/5.png"
-                            },
-                            {
-                                "id": 18,
-                                "product_name":"Peach juice",
-                                "product_price":"400",
-                                "category": "Beverages",
-                                "image": "/images/1.png"
-                            }
-                        ];
-
->>>>>>> 2fcd32c2a85f4b4cb0f0ef0d349e73c0f45fb15c
 function Beverages () {
     const [beverageProducts, setBeverageProducts] = useState([])
     useEffect(() => {
